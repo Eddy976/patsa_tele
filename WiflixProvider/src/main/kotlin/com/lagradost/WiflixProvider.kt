c8687e9ff3ca57c211843e01dcc2745b6b26c2df
Eddy976 <47984460+Eddy976@users.noreply.github.com>
2024-10-17 19:28:39 +0200

    Update WiflixProvider.kt

--- WiflixProvider/src/main/kotlin/com/lagradost/WiflixProvider.kt
+++ WiflixProvider/src/main/kotlin/com/lagradost/WiflixProvider.kt
@@ -15,7 +15,7 @@ import java.util.*
 
 class WiflixProvider : MainAPI() {
     override var mainUrl = "https://wiflix-catalogue.lol"
-    override var name = "Wiflix"
+    override var name = "Wiflix-cat"
     override val hasQuickSearch = false // recherche rapide (optionel, pas vraimet utile)
     override val hasMainPage = true // page d'accueil (optionel mais encoragé)
     override var lang = "fr" // fournisseur est en francais
