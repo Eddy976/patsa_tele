840ae7690dce9bf15cb3303fb42f915d7d5bd497
Eddy976 <47984460+Eddy976@users.noreply.github.com>
2025-04-13 11:09:13 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,16 +5,13 @@ buildscript {
     repositories {
         google()
         mavenCentral()
-        // Shitpack repo which contains our tools and dependencies
         maven("https://jitpack.io")
     }
 
     dependencies {
-        //classpath("com.android.tools.build:gradle:7.0.4")
-	classpath("com.android.tools.build:gradle:7.4.2") 
-        // Cloudstream gradle plugin which makes everything work and builds plugins
+        classpath("com.android.tools.build:gradle:7.4.2") // AGP compatible avec compileSdk 33
         classpath("com.github.recloudstream:gradle:master-SNAPSHOT")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.0") // Kotlin 2.1.0
     }
 }
 
@@ -38,12 +35,12 @@ subprojects {
     apply(plugin = "com.lagradost.cloudstream3.gradle")
 
     cloudstream {
-        // when running through github workflow, GITHUB_REPOSITORY should contain current repository name
         setRepo(System.getenv("GITHUB_REPOSITORY") ?: "user/repo")
     }
 
     android {
         compileSdkVersion(33)
+        namespace = "com.lagradost"
 
         defaultConfig {
             minSdk = 21
@@ -57,8 +54,7 @@ subprojects {
 
         tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
             kotlinOptions {
-                jvmTarget = "1.8" // Required
-                // Disables some unnecessary features
+                jvmTarget = "1.8"
                 freeCompilerArgs = freeCompilerArgs +
                         "-Xno-call-assertions" +
                         "-Xno-param-assertions" +
@@ -71,22 +67,15 @@ subprojects {
         val apk by configurations
         val implementation by configurations
 
-        // Stubs for all Cloudstream classes
         apk("com.lagradost:cloudstream3:pre-release")
 
-        // these dependencies can include any of those which are added by the app,
-        // but you dont need to include any of them if you dont need them
-        // https://github.com/recloudstream/cloudstream/blob/master/app/build.gradle
-        implementation(kotlin("stdlib")) // adds standard kotlin features, like listOf, mapOf etc
-        implementation("com.github.Blatzar:NiceHttp:0.3.2") // http library
+        implementation("org.jetbrains.kotlin:kotlin-stdlib:2.1.0")
+        implementation("com.github.Blatzar:NiceHttp:0.3.2")
         implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.1")
-        implementation("org.jsoup:jsoup:1.13.1") // html parser
-        implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4") // html parser
-
-        //run JS
+        implementation("org.jsoup:jsoup:1.13.1")
+        implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
         implementation("org.mozilla:rhino:1.7.14")
-		    // Library/extensions searching with Levenshtein distance
-        implementation ("me.xdrop:fuzzywuzzy:1.4.0")
+        implementation("me.xdrop:fuzzywuzzy:1.4.0")
     }
 }
 
